f144d1c5441953f54386b2d4992fd3de6a75a998
robiwankenobi <rfnkurle@gmail.com>
2019-03-03 16:59:55 -0800

    Psychic-Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//computer chooses a letter
+var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+    
+var wins = 0; 
+var losses = 0;
+var guesses = 9;
+var lettersYouGuessed = [];
+
+//user guesses a letter
+document.onkeyup = function () {
+    console.log("button pushed!")
+   
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();//takes keycode of key pressed and turns it into string, can use (event.key)
+    console.log(userGuess)//<---make sure buttons work
+
+    var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
+    lettersYouGuessed.push(userGuess)//puts user guess into letters you guessed array
+
+    //if user's guess matches computer guess:
+//1 is added to "Wins"
+    if (userGuess === compGuess) {
+        alert("YOU WIN. WOOHOO");
+        wins++;
+        guesses = 9;
+        lettersYouGuessed.length = 0;
+
+
+    } else if (guesses === 0) {
+        losses++;
+        alert("TOO BAD. SO SAD. YOU LOSE")
+        guesses = 9;
+        lettersYouGuessed.length = 0;
+    } 
+    else if (userGuess !== compGuess) {
+        guesses--;
+
+    } 
+
+    var html = "<h1>Psychic Game</h1>" +
+
+"<p>Guess what letter I'm thinking of</p>"+
+"<p>Wins: " + wins + "</p>" +
+"<p>Losses: " + losses +"</p>" +
+"<p>Guesses Left: " + guesses + "</p>" +
+"<p>Letters You've Guessed: " + lettersYouGuessed + "</p>";
+
+document.querySelector("#psychic-game").innerHTML = html;
+}
+
+
